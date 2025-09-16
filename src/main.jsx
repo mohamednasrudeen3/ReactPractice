91d5eaea6c366174af56f4dbd9a193ced19cf929
mohamednasrudeen3 <mohamednasrudeen21@gmail.com>
2025-09-17 00:21:06 +0530

    Learned createRoot

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import {createRoot}
-import './index.css'
-import App from './App.jsx'
+import {createRoot} from "react-dom/client"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+// const root = createRoot(document.getElementById('root'));
+// root.render(
+//   <h1>Heollo</h1>
+// )
+
+const root = createRoot(document.querySelector("#root"));
+root.render(
+  <p>
+    Hi Iam Nasrudeen
+  </p>
 )
